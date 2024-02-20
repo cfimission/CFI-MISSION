7784b12b6d8034ba6ae1ed1f619313cdffe162a4
shaikhazrath <shaikhazrathali123@gmail.com>
2024-02-20 19:33:05 +0530

    colors change

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -78,7 +78,7 @@ const Contact = () => {
 
         {/* contact form */}
 
-        <div>
+        <div id="from">
           <ContactForm />
         </div>
 
--- components/prayerrequest.jsx
+++ components/prayerrequest.jsx
@@ -7,7 +7,7 @@ const PrayerRequest = () => {
         <h1 className="text-4xl font-bold">Prayer Request</h1>
         <Link
           type="button"
-          href='/contact'
+          href='/contact#from'
           className="text-white bg-gradient-to-r from-purple-400 via-purple-500 to-purple-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-20 py-2.5 text-center me-2 mb-2 animate-bounce"
         >
           Send
